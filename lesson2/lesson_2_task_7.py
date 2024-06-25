b94e4f679931904ab85afd3e980206ac92511ed4
TinaCher <boykova.tatiana@gmail.com>
2024-06-26 00:46:49 +0300

    Additional edits

--- lesson2/lesson_2_task_7.py
+++ lesson2/lesson_2_task_7.py
@@ -1,12 +1,9 @@
 lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
 total_sum = sum(lst)
 print(total_sum)
-
-
-
+# another version
 lst = [11, 5, 8, 32, 15, 3, 20, 132, 21, 4, 555, 9, 20]
 total_sum = 0
 for number in lst:
     total_sum += number
-
 print("Sum of all elements:", total_sum)
